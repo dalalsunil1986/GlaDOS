963446431f8270f9e4606c4b2e13e06d63bf3999
Sjaak <dickio2001@gmail.com>
2020-05-14 18:04:57 +0200

    Color customization for writing

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,29 +1,30 @@
-void write_string( int colour, const char *string )
+void write_string(int foreground, int background, const char *string )
 {
     volatile char *video = (volatile char*)0xB8000;
     while( *string != 0 )
     {
         *video++ = *string++;
-        *video++ = colour;
+		// The four first bits are used for the foreground color
+		// The 3 last are used for background coloring so the bit shift and then
+		// applying a or to the two gives us the desired fore and background color code
+        *video++ = foreground ^ (background << 4);
     }
 }
 
 //80x25
-void black_screen() {
+void clear_screen() {
 	char* mem = (char*)0xb8000;
-	for (int row = 0; row < 25; row++){
-		for (int col = 0; col < 80; col++){
+	for (unsigned int row = 0; row < 25; row++){
+		for (unsigned int col = 0; col < 80; col++){
 			*mem++ = ' ';
-			*mem++ = 7;
+			*mem++ = 0x1F;
 		}
 	}
 }
 
 void _start(){
-	
-	//0xb8000 + 2 * (row * 80 + col)
-	write_string(14, "Dit is een test!");
 
-	black_screen();
+	clear_screen();
+	write_string(15,1, "Dit is een test!");
 
 }
