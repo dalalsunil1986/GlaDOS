c1da7044ae2c14493d517ac70360008b10daeeb6
MandiRex <dickio2001@gmail.com>
2020-05-14 10:58:12 +0200

    New Bootsector for 32bit Protected Mode

--- boot/print_string.asm
+++ boot/print_string.asm
@@ -1,14 +1,34 @@
-print_string:	
-	pusha		
-	mov ah, 0Eh		
+print:
+    pusha
 
-.repeat:
-	lodsb			
-	cmp al, 0
-	je .done		
-	int 10h			
-	jmp .repeat
 
-.done:
-	popa
-	ret
+start:
+    mov al, [bx] 
+    cmp al, 0 
+    je done
+
+    
+    mov ah, 0x0e
+    int 0x10
+
+   
+    add bx, 1
+    jmp start
+
+done:
+    popa
+    ret
+
+
+
+print_nl:
+    pusha
+    
+    mov ah, 0x0e
+    mov al, 0x0a 
+    int 0x10
+    mov al, 0x0d 
+    int 0x10
+    
+    popa
+    ret
