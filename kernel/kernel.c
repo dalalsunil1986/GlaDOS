f06aa2a6e6a4144cdf63cc247fddcddca8cb6088
Sjaak <dickio2001@gmail.com>
2020-05-14 17:15:34 +0200

    Clear Screen functionality

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -9,11 +9,21 @@ void write_string( int colour, const char *string )
 }
 
 //80x25
+void black_screen() {
+	char* mem = (char*)0xb8000;
+	for (int row = 0; row < 25; row++){
+		for (int col = 0; col < 80; col++){
+			*mem++ = ' ';
+			*mem++ = 7;
+		}
+	}
+}
 
 void _start(){
-
-	char* mem = 0xb8000;
+	
 	//0xb8000 + 2 * (row * 80 + col)
-	write_string(14, "KAAAS!!");
+	write_string(14, "Dit is een test!");
+
+	black_screen();
 
 }
