7d03d8541a1870c3dfc15ef917fb89e59dba87ef
Sjaak <dickio2001@gmail.com>
2020-05-15 09:29:31 +0200

    Added overwrites for byte, word and long

--- source/utils.c
+++ source/utils.c
@@ -2,6 +2,27 @@
 // This saves on function calls making it a bit more efficient
 
 // For more info on the inline asm see: https://wiki.osdev.org/Inline_Assembly
+static inline uint8_t farpeekb(uint16_t sel, void* off) {
+    uint8_t ret;
+    asm("push %%fs\n\t"
+        "mov %1, %%fs\n\t"
+        "mov %%fs:(%2),%0\n\t"
+        "pop %%fs"
+        : "=r" (ret) : "g" (sel), "r" (off)
+    );
+    return ret;
+}
+
+static inline uint16_t farpeekw(uint16_t sel, void* off) {
+    uint16_t ret;
+    asm("push %%fs\n\t"
+        "mov %1, %%fs\n\t"
+        "mov %%fs:(%2),%0\n\t"
+        "pop %%fs"
+        : "=r" (ret) : "g" (sel), "r" (off)
+    );
+    return ret;
+}
 
 static inline uint32_t farpeekl(uint16_t sel, void* off) {
     uint32_t ret;
@@ -14,7 +35,7 @@ static inline uint32_t farpeekl(uint16_t sel, void* off) {
     return ret;
 }
 
-static inline void farpokeb(uint16_t sel, void* off, uint8_t v){
+static inline void farpokeb(uint16_t sel, void* off, uint8_t val){
     asm("push %%fs\n\t"
         "mov %0, %%fs\n\t",
         "movb %2, %%fs:(%1)\n\t"
@@ -23,12 +44,51 @@ static inline void farpokeb(uint16_t sel, void* off, uint8_t v){
     );
 }
 
+static inline void farpokew(uint16_t sel, void* off, uint16_t val){
+    asm("push %%fs\n\t"
+        "mov %0, %%fs\n\t",
+        "movb %2, %%fs:(%1)\n\t"
+        "pop %%fs"
+        : : "g"(sel),  "r" (off), "r"(v)
+    );
+}
+
+static inline void farpokel(uint16_t sel, void* off, uint32_t val){
+    asm("push %%fs\n\t"
+        "mov %0, %%fs\n\t",
+        "movb %2, %%fs:(%1)\n\t"
+        "pop %%fs"
+        : : "g"(sel),  "r" (off), "r"(v)
+    );
+}
+
+
 static inline void outb(uint16_t port, uint8_t val) {
+    asm volatile("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline void outw(uint16_t port, uint16_t val) {
+    asm volatile("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline void outl(uint16_t port, uint32_t val) {
     asm volatile("outb %0, %1" : : "a"(val), "Nd"(port));
 }
 
 static inline uint8_t inb(uint16_t port){
     uint8_t ret;
+    asm volatile("inb %1, %0": "=a"(ret) : "Nd" (port));
+    return ret;
+}
+
+static inline uint8_t inw(uint16_t port){
+    uint16_t ret;
+    asm volatile("inb %1, %0": "=a"(ret) : "Nd" (port));
+    return ret;
+}
+
+static inline uint8_t inl(uint16_t port){
+    uint32_t ret;
     asm volatile("inb %1, %0": "=a"(ret) : "Nd" (port));
     return ret;
 }
