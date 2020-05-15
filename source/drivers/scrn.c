6ca5019bc5077d16c247eebbc3666b8416e1470e
Joey <jf.staps@outlook.com>
2020-05-15 16:21:34 +0200

    Commented kernel

--- source/drivers/scrn.c
+++ source/drivers/scrn.c
@@ -39,9 +39,6 @@ int strlen(const char *str)
         str++;
     }
     return count;
-    /* This loops through character array 'str', returning how
-    *  many characters it needs to check before it finds a 0.
-    *  In simple words, it returns the length in bytes of a string */
 }
 
 //Scrolls the screen
--- source/kernel/kernel.c
+++ source/kernel/kernel.c
@@ -3,9 +3,18 @@
 
 //80x25
 
+/* Commands */
+// cls(); 																clears screen
+// settextcolor(unsigned char forecolor, unsigned char backcolor);		Set foreground and background color
+// init_video(); 														Sets our text-mode VGA pointer, then clears the screen
+// print_char(unsigned char c);											Write a single charater to screen
+// print_string(unsigned char *text);									Write multiple charater to screen
+
 void _start(){
 	init_video();
-	settextcolor(15,0);
+	settextcolor(2,0);
 	print_string("Hey!");
+	settextcolor(7,5);
+	cls();
 
 }
