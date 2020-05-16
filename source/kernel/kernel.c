457df64079e0986f4be48ddf33fa66897ce99e9c
Sjaak <dickio2001@gmail.com>
2020-05-16 18:01:30 +0200

    Cosmetic change

--- source/kernel/kernel.c
+++ source/kernel/kernel.c
@@ -19,6 +19,12 @@
 void _start(){
 	install_isr();
 
+	// this functions enables the interrupts needed for keyboard input asm sti
+	enable_interrupts();
+
+	init_keyboard();
+	init_timer((uint32)50);
+
 	init_video();
 	settextcolor(14,0);
 	print_string("Booted GlaDOS successfully!\n");
@@ -29,15 +35,12 @@ void _start(){
 	print_string("                       | |_\\ \\ | (_| | |/ /\\ \\_/ /\\__/ /         \n");
 	print_string("                        \\____/_|\\__,_|___/  \\___/\\____/          \n");
 	fill_line('=',_WITDH);
+	waitForKey();
 	cls();
 	windowcolor(14,0,14,0);
 	create_window(45,_HEIGHT - 1,1,0);
 	create_window(31,12,47,0);
 
 
-	// this functions enables the interrupts needed for keyboard input asm sti
-	enable_interrupts();
 
-	init_keyboard();
-	init_timer((uint32)50);
 }
