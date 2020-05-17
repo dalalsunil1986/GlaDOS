424501ca6456661aace997ae317c9e906f0c59d0
Sjaak <dickio2001@gmail.com>
2020-05-17 11:32:41 +0200

    Code cleanup:

--- source/drivers/scrn.h
+++ source/drivers/scrn.h
@@ -1,13 +1,13 @@
 #ifndef __SCRN_H_
 #define __SCRN_H_
 
-extern void cls();
-extern void settextcolor(unsigned char forecolor, unsigned char backcolor);
-extern void init_video();
-extern void print_char( char c);
-extern void print_string( char *text);
-extern void fill_line(char c, int width);
-extern void create_window(int witdh, int height,int xOffset, int yOffset);
-extern void windowcolor(unsigned int borderFore, unsigned int borderBack,unsigned int fillFore, unsigned int fillBack);
+void cls();
+void settextcolor(unsigned char forecolor, unsigned char backcolor);
+void init_video();
+void print_char( char c);
+void print_string( char *text);
+void fill_line(char c, int width);
+void create_window(int witdh, int height,int xOffset, int yOffset);
+void windowcolor(unsigned int borderFore, unsigned int borderBack,unsigned int fillFore, unsigned int fillBack);
 
 #endif
